9b98ce1a4ad73e794895d37e1660ca959c3b7eed
joaovtmarques <jvsilvam@outlook.com>
2022-04-12 21:01:58 -0300

    create voice recognition

--- frontend/boardclass/src/pages/Home/index.js
+++ frontend/boardclass/src/pages/Home/index.js
@@ -1,19 +1,57 @@
 import { Link } from "react-router-dom";
-import React from 'react';
-import mic from './images/mic.png';
+import React, { useRef, useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+
 import styles from './home.module.css';
 
+import mic from './images/mic.png';
+import mic_red from './images/mic_red.png';
 
 export const Home = () => {
+    const { transcript, resetTranscript } = useSpeechRecognition();
+    const [isListening, setIsListening] = useState(false);
+    const microphoneRef = useRef(null);
+
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+        return (
+          <div className={styles.notSupportContainer}>
+            Browser is not Support Speech Recognition.
+          </div>
+        );
+    }
+
+    const handleListening = () => {
+        setIsListening(true);
+        microphoneRef.current.classList.add("listening");
+        SpeechRecognition.startListening({
+            continuous: true,
+        });
+    };
+
+    const stopListening = () => {
+        setIsListening(false);
+        microphoneRef.current.classList.remove("listening");
+        SpeechRecognition.stopListening();
+    }
+
     return(
        <body>
            <div className={styles.containerHome}>
-                <div className={styles.mic}>
-                    <input className={styles.micButton} type="image" src={mic} width={125} height={99}></input>
+                <div className={styles.mic} ref={microphoneRef} onClick={handleListening}>
+                    { isListening &&
+                        <img className={styles.micButton} src={mic_red} alt="microphone"/>
+                        ||
+                        <img className={styles.micButton} src={mic} alt="microphone" />
+                    }
                 </div>
 
                 <div className={styles.help}>
-                    <input className={styles.helpInput} type="text" placeholder='Olá professor(a) como posso te ajudar?'/>  
+                    <input className={styles.helpInput} type="text" placeholder={'Olá professor(a) como posso te ajudar?'} value={transcript} />
+                    { isListening &&
+                        <button className={styles.stopButton} onClick={stopListening}></button>
+                        ||
+                        null
+                    }
                 </div>
 
                 <div className={styles.centralHelp}>
