04854aa329b553b220e3e006abcf6c344e2540a6
joaovtmarques <jvsilvam@outlook.com>
2022-04-13 23:18:40 -0300

    add popup & configure speech recogntion & set favicon

--- frontend/boardclass/src/pages/Home/index.js
+++ frontend/boardclass/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import Alert from 'react-popup-alert'
 
 import styles from './home.module.css';
 
@@ -11,6 +12,33 @@ export const Home = () => {
     const { transcript, resetTranscript } = useSpeechRecognition();
     const [isListening, setIsListening] = useState(false);
     const microphoneRef = useRef(null);
+    const [alert, setAlert] = useState({
+        type: 'warning',
+        text: 'alert message',
+        show: false
+      })
+    
+    function onCloseAlert() {
+        setAlert({
+            type: '',
+            text: '',
+            show: false
+        })
+    }
+
+    function onShowAlert(type) {
+        setAlert({
+            type: type,
+            text: 'olá! sou a assistente BoardClass. você pode pressionar o botão azul ao lado e me dar um comando por voz! ;)',
+            show: true
+        })
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            onShowAlert('warning')
+        }, 1500)
+    }, [])
 
     if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
         return (
@@ -36,17 +64,70 @@ export const Home = () => {
 
     return(
        <body>
-           <div className={styles.containerHome}>
-                <div className={styles.mic} ref={microphoneRef} onClick={handleListening}>
+           <div className={styles.alertContainer}>
+                <Alert
+                    header={''}
+                    btnText={'Entendi :)'}
+                    text={alert.text}
+                    type={alert.type}
+                    show={alert.show}
+                    onClosePress={onCloseAlert}
+                    pressCloseOnOutsideClick={true}
+                    showBorderBottom={true}
+                    alertStyles={{
+                        height: 160,
+                        width: 200,
+                        backgroundColor: '#1ABBBB',
+                        padding: 15,
+                        borderRadius: 15,
+                        border: 0,
+                        textAlign: 'center',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                    }}
+                    headerStyles={{
+                        display: 'none'
+                    }}
+                    textStyles={{
+                        textAlign: 'center',
+                        fontSize: 14,
+                        fontWeight: '300',
+                        fontFamily: 'sans-serif',
+                        color: '#FFFFFF',
+                        padding: 0,
+                    }}
+                    buttonStyles={{
+                        textAlign: 'center',
+                        paddingTop: 10,
+                        paddingBottom: 10,
+                        paddingRight: 15,
+                        paddingLeft: 15,
+                        fontSize: 14,
+                        fontWeight: '600',
+                        fontFamily: 'sans-serif',
+                        color: '#FFFFFF',
+                        borderRadius: 10,
+                        textDecoration: 'none'
+                    }}
+                />
+            </div>
+            <div className={styles.containerHome}>
+                <button className={styles.mic} ref={microphoneRef} onClick={isListening ? stopListening : handleListening}>
                     { isListening &&
                         <img className={styles.micButton} src={mic_red} alt="microphone"/>
                         ||
                         <img className={styles.micButton} src={mic} alt="microphone" />
                     }
-                </div>
+                </button>
 
                 <div className={styles.help}>
-                    <input className={styles.helpInput} type="text" placeholder={'Olá professor(a) como posso te ajudar?'} value={transcript} />
+                    <input 
+                        className={styles.helpInput} 
+                        type="text" placeholder={'Olá professor(a) como posso te ajudar?'} 
+                        value={transcript}
+                    />
                     { isListening &&
                         <button className={styles.stopButton} onClick={stopListening}></button>
                         ||
